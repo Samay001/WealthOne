b1d2c7654e66d729a0f101696afbc68ebda6426b
Samay001 <samayrathod1@gmail.com>
2025-03-21 18:00:42 +0530

    added starter files

---
+++ backend/wealth-one/src/main/java/com/backend/wealth_one/WealthOneApplication.java
@@ -0,0 +1,13 @@
+package com.backend.wealth_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthOneApplication.class, args);
+	}
+
+}
---
+++ backend/wealth-one/src/test/java/com/backend/wealth_one/WealthOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.wealth_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
