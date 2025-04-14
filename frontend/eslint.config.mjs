8291764515b7b2a994bb8b49e91001e8f0a6e1d7
Samay001 <samayrathod1@gmail.com>
2025-04-14 22:54:28 +0530

    updated eslint config file

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,8 +9,21 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+
+// Define ESLint configuration
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Add custom rules here
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react/no-unescaped-entities": "off",
+      "prefer-const": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
+
 
 export default eslintConfig;
